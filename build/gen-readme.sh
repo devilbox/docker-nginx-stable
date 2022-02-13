b70c80a33b97e05ed33c93acdf286a4ec1abfcc7
cytopia <cytopia@everythingcli.org>
2022-02-13 16:31:10 +0100

    Fix gen-readme

--- build/gen-readme.sh
+++ build/gen-readme.sh
@@ -6,11 +6,12 @@
 ###
 CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)/.."
 IMAGE="${1}"
+ARCH="${2}"
 
 ###
 ### Retrieve information afterwards and Update README.md
 ###
-INFO="$( docker run --rm --entrypoint=nginx "${IMAGE}" -v 2>&1 | tr -d '\r' )"
+INFO="$( docker run --rm --platform "${ARCH}" --entrypoint=nginx "${IMAGE}" -v 2>&1 | tr -d '\r' )"
 
 echo "${INFO}"
 sed -i'' '/##[[:space:]]Version/q' "${CWD}/README.md"
