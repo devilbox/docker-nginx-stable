7cd6c62fe1356f34065f9097ac30281dfac2e33d
cytopia <cytopia@everythingcli.org>
2022-12-23 02:20:36 +0100

    Fix mass vhost tests

--- tests/220-mass-vhost__reverse-proxy.sh
+++ tests/220-mass-vhost__reverse-proxy.sh
@@ -46,7 +46,7 @@ NAME_RPROXY2="$( get_random_name )"
 ###
 ### GLOBALS
 ###
-DOCROOT="htdocs"
+#DOCROOT="htdocs"
 TLD=".loc"
 MOUNT_CONT="/shared/httpd"
 MOUNT_HOST="$( tmp_dir )"
@@ -64,11 +64,10 @@ APP1_NAME="my-project-1"
 APP1_TXT="hello from ${APP1_NAME} via httpd with NodeJS"
 APP1_URL="http://localhost:${HOST_PORT_HTTP}"
 APP1_HDR="Host: ${APP1_NAME}${TLD}"
+APP1_PORT=3000
 
-mkdir -p "${MOUNT_HOST}/${APP1_NAME}/cfg"
-echo "conf:rproxy:http:${NAME_RPROXY1}:3000" > "${MOUNT_HOST}/${APP1_NAME}/cfg/backend.txt"
-mkdir -p "${MOUNT_HOST}/${APP1_NAME}/${DOCROOT}"
-cat << EOF > "${MOUNT_HOST}/${APP1_NAME}/${DOCROOT}/app.js"
+APP1_DIR="$( tmp_dir )"
+cat << EOF > "${APP1_DIR}/app.js"
 const http = require('http');
 const server = http.createServer((req, res) => {
         res.statusCode = 200;
@@ -77,8 +76,15 @@ const server = http.createServer((req, res) => {
         res.write('${APP1_TXT}\n');
         res.end();
 });
-server.listen(3000, '0.0.0.0');
+server.listen(${APP1_PORT}, '0.0.0.0');
 EOF
+
+# Create Project for Application 1
+mkdir -p "${MOUNT_HOST}/${APP1_NAME}/cfg"
+echo "conf:rproxy:http:${NAME_RPROXY1}:${APP1_PORT}" > "${MOUNT_HOST}/${APP1_NAME}/cfg/backend.txt"
+
+
+
 
 ###
 ### Application 2
@@ -87,6 +93,23 @@ APP2_NAME="another-nodejs-app"
 APP2_TXT="hello hello from ${APP2_NAME} via httpd with NodeJS"
 APP2_URL="http://localhost:${HOST_PORT_HTTP}"
 APP2_HDR="Host: ${APP2_NAME}${TLD}"
+APP2_PORT=4000
+
+APP2_DIR="$( tmp_dir )"
+cat << EOF > "${APP2_DIR}/app.js"
+const http = require('http');
+const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.write('[OK]\n');
+        res.write('${APP2_TXT}\n');
+        res.end();
+});
+server.listen(${APP2_PORT}, '0.0.0.0');
+EOF
+
+
+
 
 
 
@@ -100,19 +123,19 @@ APP2_HDR="Host: ${APP2_NAME}${TLD}"
 run "docker pull --platform linux/amd64 node:19-alpine"
 
 ###
-### Start Node-1 Container
+### Start Node-1 Container (tcp 3000)
 ###
 run "docker run -d --name ${NAME_RPROXY1} \
--v ${MOUNT_HOST}:${MOUNT_CONT} \
-node:19-alpine node ${MOUNT_CONT}/${APP1_NAME}/${DOCROOT}/app.js >/dev/null"
+-v ${APP1_DIR}:/app \
+node:19-alpine node /app/app.js >/dev/null"
 
 
 ###
-### Start Node-2 Container
+### Start Node-2 Container (tcp 4000)
 ###
 run "docker run -d --name ${NAME_RPROXY2} \
--v ${MOUNT_HOST}:${MOUNT_CONT} \
-node:19-alpine sh -c 'sleep 10; node ${MOUNT_CONT}/${APP2_NAME}/${DOCROOT}/app.js' >/dev/null"
+-v ${APP2_DIR}:/app \
+node:19-alpine node /app/app.js >/dev/null"
 
 
 ###
@@ -156,20 +179,9 @@ fi
 ###
 ### Test: APP2
 ###
+# Create Project for Application 2
 mkdir -p "${MOUNT_HOST}/${APP2_NAME}/cfg"
-echo "conf:rproxy:http:${NAME_RPROXY2}:4000" > "${MOUNT_HOST}/${APP2_NAME}/cfg/backend.txt"
-mkdir -p "${MOUNT_HOST}/${APP2_NAME}/${DOCROOT}"
-cat << EOF > "${MOUNT_HOST}/${APP2_NAME}/${DOCROOT}/app.js"
-const http = require('http');
-const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.write('[OK]\n');
-        res.write('${APP2_TXT}\n');
-        res.end();
-});
-server.listen(4000, '0.0.0.0');
-EOF
+echo "conf:rproxy:http:${NAME_RPROXY2}:${APP2_PORT}" > "${MOUNT_HOST}/${APP2_NAME}/cfg/backend.txt"
 
 if ! test_vhost_response "${APP2_TXT}" "${APP2_URL}" "${APP2_HDR}"; then
 	docker_logs "${NAME_RPROXY2}"
